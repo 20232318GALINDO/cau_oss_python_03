bfe9909a919011eb5fa5b8b8b35623da8082de62
윈레드 <127921886+20232318GALINDO@users.noreply.github.com>
2023-03-16 22:36:27 +0900

    Update volume_calc.py

--- volume_calc.py
+++ volume_calc.py
@@ -1,7 +1,7 @@
-width = float(input("가로: "))
+width  = float(input("가로: "))
 length = float(input("세로: "))
 height = float(input("높이: "))
 
-부피 = width * length * height
+volume = width * length * height
 
-print("박스의 부피는", 부피, "입니다.")
+print("박스의 부피는", volume, "입니다.")
